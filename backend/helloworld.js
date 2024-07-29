823fcfce384f622f05a58a197307efc439fc82c0
ggarang3 <ggarang24@gmail.com>
2024-07-29 11:42:07 +0800

    module 1-3

--- backend/helloworld.js
+++
@@ -1,7 +0,0 @@
-let helloText = "say Hi";
-let check = 4;
-if (check > 3) {
-let hello = "say Hello instead";
-console.log(hello); // "say Hello instead"
-}
-console.log(hello); // hello is not defined
